760c60f0ba49d841559c32103741c384e3ae3ab9
Aditya Shukla <Shadowshukla76@gmail.com>
2025-06-21 08:45:54 +0530

    addd optimal approach

--- binary_serach/easy/upper_bound.cpp
+++ binary_serach/easy/upper_bound.cpp
@@ -14,12 +14,32 @@ int upperBound(vector<int> arr,int x,int n)
 	return n;
 }
 
+int upperBoundOptimal(vector<int> arr,int x,int n)
+{
+	int low=0;
+	int ans=n;
+	int high=n-1;
+	while(low<=high)
+	{
+		int mid=(low+high)/2;
+		if (arr[mid]>x)
+		{
+			ans=mid;
+			high=mid-1;
+		}else
+		{
+			low=mid+1;
+		}
+	}
+	return ans;
+}
+
 
 int main()
 {
 	vector<int> arr = {3, 5, 8, 9, 15, 19};
 	int n = 6, x = 9;
-	int ind = upperBound(arr, x, n);
+	int ind = upperBoundOptimal(arr, x, n);
 	cout << "The upper bound is the index: " << ind << "\n";
 	return 0;
 }
